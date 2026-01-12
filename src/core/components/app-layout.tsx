44be49fe07a2f6bc3df84d1f0636d99bb4a4e3ae
Célestin Ballèvre <celestin.ballevre@protonmail.com>
2026-01-12 16:36:26 +0100

    feat: Improve identity site

--- src/core/components/app-layout.tsx
+++ src/core/components/app-layout.tsx
@@ -1,8 +1,19 @@
 import type { PropsWithChildren, FC } from 'react';
-import { ThemedLayout } from '@refinedev/mui';
+import { ThemedLayout, ThemedTitle } from '@refinedev/mui';
 
 const AppLayout: FC<PropsWithChildren> = ({ children }) => {
-  return <ThemedLayout>{children}</ThemedLayout>;
+  return (
+    <ThemedLayout
+      Title={({ collapsed }) => (
+        <ThemedTitle
+          collapsed={collapsed}
+          text={import.meta.env.VITE_APP_ORGANIZATION}
+        />
+      )}
+    >
+      {children}
+    </ThemedLayout>
+  );
 };
 
 export { AppLayout };
