bb5b6a2a424e57abadaf1891ca97348faf88c30c
Célestin Ballèvre <celestin.ballevre@protonmail.com>
2026-01-12 16:45:07 +0100

    feat: Update dashboard

--- src/dashboard/pages/index.tsx
+++ src/dashboard/pages/index.tsx
@@ -1,5 +1,17 @@
+import { Stack, Typography } from "@mui/material";
+import { useTranslate } from "@refinedev/core";
+
 const DashboardPage = () => {
-  return <div>Welcome to the Dashboard</div>;
+  const t = useTranslate();
+
+  return <Stack spacing={2}>
+    <Typography variant="h4" component="h1">
+      {t("dashboard.welcome")}
+    </Typography>
+    <Typography variant="body1">
+      {t("dashboard.description")}
+    </Typography>
+  </Stack>;
 };
 
 export { DashboardPage };
