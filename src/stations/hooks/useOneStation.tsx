74f6e1c1be507c99541ca8af3e6c7c7f76b9e0b6
Célestin Ballèvre <celestin.ballevre@protonmail.com>
2026-01-13 10:45:48 +0100

    refactor: Use a custom hook to get a station and their instrument

---
+++ src/stations/hooks/useOneStation.tsx
@@ -0,0 +1,51 @@
+import type { Instrument, Station } from '@/shared/types/models';
+import { useList, useMany, useOne, type HttpError } from '@refinedev/core';
+
+
+const useOneStation = ({ id }: { id: string | undefined }) => {
+
+  const {
+    result: station,
+    query: { isLoading, isError }
+  } = useOne<Station, HttpError>({
+    resource: 'stations',
+    id: id,
+    queryOptions: {
+      enabled: !!id,
+    },
+  });
+
+  const {
+    result: stationInstruments,
+  } = useList({
+    resource: 'station_has_instruments',
+    filters: [
+      {
+        field: 'station_id',
+        operator: 'eq',
+        value: id,
+      },
+    ],
+    pagination: { pageSize: 100 },
+    queryOptions: {
+      enabled: !!id,
+    },
+  });
+
+  // Get instruments associated with the station
+  const instrumentIds =
+    stationInstruments?.data?.map((rel) => rel.instrument_id) || [];
+  const {
+    result: { data: instruments },
+    query: { isLoading: isInstrumentsLoading, isError: isInstrumentsError },
+  } = useMany<Instrument>({
+    resource: 'instruments',
+    ids: instrumentIds,
+    queryOptions: { enabled: instrumentIds.length > 0 },
+  });
+
+  return { station: station, instruments, isLoading, isError, isInstrumentsLoading, isInstrumentsError };
+
+};
+
+export { useOneStation };
