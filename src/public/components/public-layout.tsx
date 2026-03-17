ea7f40c10f219b5dffd2a113a5cff07b4c815b06
Célestin Ballèvre <celestin.ballevre@protonmail.com>
2026-03-17 11:50:34 +0100

    refactor: Rename NavBar to PublicNavBar

--- src/public/components/public-layout.tsx
+++ src/public/components/public-layout.tsx
@@ -1,7 +1,7 @@
 import { Container } from '@mui/material';
 import type { FC, PropsWithChildren } from 'react';
 
-import { NavBar } from '@/public/components/nav-bar';
+import { PublicNavBar } from '@/public/components/public-nav-bar';
 
 interface PublicLayoutProps extends PropsWithChildren {
   isFluid?: boolean;
@@ -10,7 +10,7 @@ interface PublicLayoutProps extends PropsWithChildren {
 const PublicLayout: FC<PublicLayoutProps> = ({ children, isFluid = false }) => {
   return (
     <>
-      <NavBar />
+      <PublicNavBar />
       <Container maxWidth={isFluid ? false : 'lg'} disableGutters={isFluid}>
         {children}
       </Container>
